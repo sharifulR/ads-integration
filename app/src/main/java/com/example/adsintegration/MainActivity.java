d4d4ce1fb658ae680ddaf5279f8cc6e048043ba8
USER <shariful15-11605@diu.edu.bd>
2022-08-23 10:44:13 +0600

    applovin interstitialAd,rewardedAd ads integration

--- app/src/main/java/com/example/adsintegration/MainActivity.java
+++ app/src/main/java/com/example/adsintegration/MainActivity.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Toast;
 
 import com.applovin.mediation.MaxAd;
 import com.applovin.mediation.MaxAdListener;
 import com.applovin.mediation.MaxError;
+import com.applovin.mediation.MaxReward;
+import com.applovin.mediation.MaxRewardedAdListener;
 import com.applovin.mediation.ads.MaxAdView;
 import com.applovin.mediation.ads.MaxInterstitialAd;
+import com.applovin.mediation.ads.MaxRewardedAd;
 import com.applovin.sdk.AppLovinSdk;
 import com.applovin.sdk.AppLovinSdkConfiguration;
 
-public class MainActivity extends AppCompatActivity implements MaxAdListener {
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements MaxAdListener, MaxRewardedAdListener {
 
     private MaxAdView adView;
     private MaxInterstitialAd interstitialAd;
+    private MaxRewardedAd rewardedAd;
+    private int retryAttempt;
     AppCompatButton button;
 
     @Override
@@ -39,17 +48,17 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener {
         adView = findViewById(R.id.adView);
         adView.loadAd();
         LoadInterstitialAd();
+        LoadMaxRewardedAdAd();
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ( interstitialAd.isReady() )
-                {
-                    interstitialAd.showAd();
+                if (rewardedAd.isReady()) {
+                    rewardedAd.showAd();
 
-                    //code here
-                    LoadInterstitialAd();
+                    // code here
+                    LoadMaxRewardedAdAd();
                 }
             }
         });
@@ -62,34 +71,69 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener {
         interstitialAd.loadAd();
     }
 
-    @Override
-    public void onAdLoaded(MaxAd ad) {
+    private void LoadMaxRewardedAdAd() {
 
+        rewardedAd = MaxRewardedAd.getInstance("YOUR_AD_UNIT_ID", this);
+        rewardedAd.setListener(this);
+        rewardedAd.loadAd();
     }
 
     @Override
-    public void onAdDisplayed(MaxAd ad) {
+    public void onAdLoaded(final MaxAd maxAd)
+    {
 
+        retryAttempt = 0;
     }
 
     @Override
-    public void onAdHidden(MaxAd ad) {
+    public void onAdLoadFailed(final String adUnitId, final MaxError error)
+    {
+        retryAttempt++;
+        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
 
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                rewardedAd.loadAd();
+            }
+        }, delayMillis );
+    }
+
+    @Override
+    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
+    {
+
+        rewardedAd.loadAd();
         interstitialAd.loadAd();
     }
 
     @Override
-    public void onAdClicked(MaxAd ad) {
+    public void onAdDisplayed(final MaxAd maxAd) {}
 
+    @Override
+    public void onAdClicked(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdHidden(final MaxAd maxAd)
+    {
+        rewardedAd.loadAd();
+        interstitialAd.loadAd();
     }
 
     @Override
-    public void onAdLoadFailed(String adUnitId, MaxError error) {
+    public void onRewardedVideoStarted(final MaxAd maxAd) {}
 
+    @Override
+    public void onRewardedVideoCompleted(final MaxAd maxAd) {
+        //users offer
+        Toast.makeText(this, "Thanks", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onAdDisplayFailed(MaxAd ad, MaxError error) {
+    public void onUserRewarded(final MaxAd maxAd, final MaxReward maxReward)
+    {
 
     }
 }
