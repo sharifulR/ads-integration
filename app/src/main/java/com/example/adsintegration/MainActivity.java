bfe2334f8acd05342be042c013edd18e50ee0c87
USER <shariful15-11605@diu.edu.bd>
2022-08-23 11:40:13 +0600

    admob banner ads integration

--- app/src/main/java/com/example/adsintegration/MainActivity.java
+++ app/src/main/java/com/example/adsintegration/MainActivity.java
@@ -8,26 +8,19 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
-import com.applovin.mediation.MaxAd;
-import com.applovin.mediation.MaxAdListener;
-import com.applovin.mediation.MaxError;
-import com.applovin.mediation.MaxReward;
-import com.applovin.mediation.MaxRewardedAdListener;
-import com.applovin.mediation.ads.MaxAdView;
-import com.applovin.mediation.ads.MaxInterstitialAd;
-import com.applovin.mediation.ads.MaxRewardedAd;
-import com.applovin.sdk.AppLovinSdk;
-import com.applovin.sdk.AppLovinSdkConfiguration;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity implements MaxAdListener, MaxRewardedAdListener {
+public class MainActivity extends AppCompatActivity {
 
-    private MaxAdView adView;
-    private MaxInterstitialAd interstitialAd;
-    private MaxRewardedAd rewardedAd;
-    private int retryAttempt;
-    AppCompatButton button;
+    private AppCompatButton button;
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,104 +29,17 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener, Ma
 
         button=findViewById(R.id.show_iv_ads);
 
-        //applovin initial
-        AppLovinSdk.getInstance(MainActivity.this).setMediationProvider("max");
-        AppLovinSdk.initializeSdk(MainActivity.this, new AppLovinSdk.SdkInitializationListener() {
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
-            public void onSdkInitialized(final AppLovinSdkConfiguration configuration) {
-
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
 
-        adView = findViewById(R.id.adView);
-        adView.loadAd();
-        LoadInterstitialAd();
-        LoadMaxRewardedAdAd();
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (rewardedAd.isReady()) {
-                    rewardedAd.showAd();
-
-                    // code here
-                    LoadMaxRewardedAdAd();
-                }
-            }
-        });
     }
 
-    private void LoadInterstitialAd() {
-
-        interstitialAd = new MaxInterstitialAd( "YOUR_AD_UNIT_ID", this );
-        interstitialAd.setListener( this );
-        interstitialAd.loadAd();
-    }
-
-    private void LoadMaxRewardedAdAd() {
-
-        rewardedAd = MaxRewardedAd.getInstance("YOUR_AD_UNIT_ID", this);
-        rewardedAd.setListener(this);
-        rewardedAd.loadAd();
-    }
-
-    @Override
-    public void onAdLoaded(final MaxAd maxAd)
-    {
-
-        retryAttempt = 0;
-    }
-
-    @Override
-    public void onAdLoadFailed(final String adUnitId, final MaxError error)
-    {
-        retryAttempt++;
-        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
-
-        new Handler().postDelayed(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                rewardedAd.loadAd();
-            }
-        }, delayMillis );
-    }
-
-    @Override
-    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
-    {
-
-        rewardedAd.loadAd();
-        interstitialAd.loadAd();
-    }
-
-    @Override
-    public void onAdDisplayed(final MaxAd maxAd) {}
-
-    @Override
-    public void onAdClicked(final MaxAd maxAd) {}
-
-    @Override
-    public void onAdHidden(final MaxAd maxAd)
-    {
-        rewardedAd.loadAd();
-        interstitialAd.loadAd();
-    }
-
-    @Override
-    public void onRewardedVideoStarted(final MaxAd maxAd) {}
-
-    @Override
-    public void onRewardedVideoCompleted(final MaxAd maxAd) {
-        //users offer
-        Toast.makeText(this, "Thanks", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onUserRewarded(final MaxAd maxAd, final MaxReward maxReward)
-    {
-
-    }
 }
