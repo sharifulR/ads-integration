05f700290c5c35b0225578826a60c2c370276091
USER <shariful15-11605@diu.edu.bd>
2022-08-23 10:29:14 +0600

    applovin interstitialAd ads integration

--- app/src/main/java/com/example/adsintegration/MainActivity.java
+++ app/src/main/java/com/example/adsintegration/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.adsintegration;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.applovin.mediation.MaxAd;
 import com.applovin.mediation.MaxAdListener;
 import com.applovin.mediation.MaxError;
 import com.applovin.mediation.ads.MaxAdView;
+import com.applovin.mediation.ads.MaxInterstitialAd;
 import com.applovin.sdk.AppLovinSdk;
 import com.applovin.sdk.AppLovinSdkConfiguration;
 
 public class MainActivity extends AppCompatActivity implements MaxAdListener {
 
     private MaxAdView adView;
+    private MaxInterstitialAd interstitialAd;
+    AppCompatButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.show_iv_ads);
 
         //applovin initial
         AppLovinSdk.getInstance(MainActivity.this).setMediationProvider("max");
@@ -31,6 +38,28 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener {
 
         adView = findViewById(R.id.adView);
         adView.loadAd();
+        LoadInterstitialAd();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( interstitialAd.isReady() )
+                {
+                    interstitialAd.showAd();
+
+                    //code here
+                    LoadInterstitialAd();
+                }
+            }
+        });
+    }
+
+    private void LoadInterstitialAd() {
+
+        interstitialAd = new MaxInterstitialAd( "YOUR_AD_UNIT_ID", this );
+        interstitialAd.setListener( this );
+        interstitialAd.loadAd();
     }
 
     @Override
@@ -46,6 +75,7 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener {
     @Override
     public void onAdHidden(MaxAd ad) {
 
+        interstitialAd.loadAd();
     }
 
     @Override
